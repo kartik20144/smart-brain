7dc32d00a2dfafc6421cf891e06392dc9ef75e53
Kartik <kartikagarwal20144@gmail.com>
2024-02-05 23:23:40 +0530

    updating fetch url

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ class App extends Component {
       .then(response => {
         if(response){
 
-          fetch('http://localhost:3000/image', {
+          fetch('https://mybackend-gu89.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component{
         this.setState({ password: event.target.value })
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://mybackend-gu89.onrender.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
